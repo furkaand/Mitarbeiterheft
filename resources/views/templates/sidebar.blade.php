699e59fca21b4e8928c4de4274b50f6436384781
Furkan <furkan@dogan.de>
2025-02-18 20:40:46 +0100

    update

--- resources/views/templates/sidebar.blade.php
+++ resources/views/templates/sidebar.blade.php
@@ -124,32 +124,33 @@
 
                         <li>
                             <!-- Current: "bg-blue-700 text-white", Default: "text-blue-200 hover:text-white hover:bg-blue-700" -->
-                            <a href="/shops" class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6
-                                        text-blue-200 hover:bg-blue-700 hover:text-white">
-
+                            
+                            
+                             <a href="{{ route('your_instructions.index') }}" class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-blue-200 hover:bg-blue-700 hover:text-white">
                                 <svg class="h-6 w-6 shrink-0 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path>
                                 </svg>
                                 Deine Unterweisungen
                             </a>
+
                         </li>
                         <li>
                             <div class="text-xs font-semibold leading-6 text-blue-200 mt-3">VERWALTEN</div>
                         </li>
                         <li>
-                            <a href="/shops" class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6
-                                        text-blue-200 hover:bg-blue-700 hover:text-white">
-                                <svg class="h-6 w-6 shrink-0 text-blue-200 group-hover:text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"></path>
+                        <li>
+                        <a href="{{ route('articles.pending') }}" class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-blue-200 hover:bg-blue-700 hover:text-white">
+                                <svg class="h-6 w-6 shrink-0 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path>
                                 </svg>
                                 News freischalten
                             </a>
                         </li>
                         <li>
-                            <a href="/logs" class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6
-                                            text-blue-200 hover:bg-blue-700 hover:text-white">
-                                <svg class="h-6 w-6 shrink-0 text-blue-200 group-hover:text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"></path>
+                        <!-- Current: "bg-blue-700 text-white", Default: "text-blue-200 hover:text-white hover:bg-blue-700" -->
+                        <a href="{{ route('all_instructions.index') }}" class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-blue-200 hover:bg-blue-700 hover:text-white">
+                                <svg class="h-6 w-6 shrink-0 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path>
                                 </svg>
                                 Alle Unterweisungen
                             </a>
--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,16 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AllInstructionsController;
+use App\Http\Controllers\YourInstructionController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 
-Route::post('/login', [AuthController::class, 'authenticate'])->name('authenticate');#
+Route::post('/login', [AuthController::class, 'authenticate'])->name('authenticate');
 Route::post('/register', [AuthController::class, 'create'])->name('create');
 
 Route::middleware(['auth', 'auth.session'])->group(function () {
@@ -17,4 +21,14 @@ Route::middleware(['auth', 'auth.session'])->group(function () {
     Route::get('/images/{filename}', [DashboardController::class, 'images'])->middleware('auth')->name('images.show');
 
     Route::resource('articles', ArticleController::class);
+    Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
+    Route::post('/images/upload', [ArticleController::class, 'uploadImage'])->name('images.upload');
+
+    Route::get('/all-instructions', [AllInstructionsController::class, 'index'])->name('all_instructions.index');
+    Route::get('/all-instructions/create', [AllInstructionsController::class, 'create'])->name('all_instructions.create');
+    Route::post('/all-instructions', [AllInstructionsController::class, 'store'])->name('all_instructions.store');
+
+    Route::get('/your-instructions', [YourInstructionController::class, 'index'])->name('your_instructions.index');
+    Route::get('/your-instructions/{id}', [YourInstructionController::class, 'show'])->name('your_instructions.show');
+    Route::put('/your-instructions/{id}', [YourInstructionController::class, 'update'])->name('your_instructions.update');
 });
